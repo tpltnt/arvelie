f1797240adb0de4b6bfe5d0fe4959930b1860653
tpltnt <1172976+tpltnt@users.noreply.github.com>
2020-05-15 00:07:15 +0200

    simple type work with chrono::Date

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,36 @@
 //! This package enables you to work with the Arvile calendar.
 extern crate chrono;
-use chrono::prelude::*;
+use chrono::Utc;
 
 /// The data structure to represent a date.
 #[derive(Debug)]
 pub struct Date {
-    date: DateTime<Utc>,
+    date: chrono::Date<Utc>,
+}
+
+impl Date {
+    /// Return date as chronos::Date
+    pub fn into_chrono(self) -> chrono::Date<Utc> {
+        self.date.clone()
+    }
+}
+
+// trait implementation
+impl From<chrono::Date<Utc>> for Date {
+    fn from(item: chrono::Date<Utc>) -> Self {
+        Date { date: item }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use chrono::TimeZone;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn from_to_chrono() {
+        let dt1 = Utc.ymd(1984, 2, 2);
+        let av1 = crate::Date::from(dt1);
+        assert_eq!(av1.into_chrono(), dt1);
     }
 }
