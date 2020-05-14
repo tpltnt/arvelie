11b18adf5a1ffe8241d5a8304bcc58f6ac077f96
tpltnt <1172976+tpltnt@users.noreply.github.com>
2020-05-14 23:10:16 +0200

    added chrono crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! This package enables you to work with the Arvile calendar.
+extern crate chrono;
+use chrono::prelude::*;
+
+/// The data structure to represent a date.
+#[derive(Debug)]
+pub struct Date {
+    date: DateTime<Utc>,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
